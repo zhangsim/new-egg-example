4ad34d3d85e16934950b708b6c627c88c59ddc47
zhangsiming <zhangsiming@yy.com>
2021-04-28 13:53:51 +0800

    egg

---
+++ app/controller/user.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars,no-undef,strict
+// class UserController extends Controller {
+//   async info() {
+//     const { ctx } = this;
+//     ctx.body = {
+//       name: ctx.params.id,
+//     };
+//   }
+// }
